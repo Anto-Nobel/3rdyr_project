3458410d765869a0dc7423cc9e68296bd2824774
PraveenKanth1802 <92263475+PraveenKanth1802@users.noreply.github.com>
2022-01-02 15:09:49 +0530

    frontend

---
+++ frontend/index.js
@@ -0,0 +1,23 @@
+ const getDatePickerTitle = elem => {
+                // From the label or the aria-label
+                const label = elem.nextElementSibling;
+                let titleText = '';
+                if (label && label.tagName === 'LABEL') {
+                    titleText = label.textContent;
+                } else {
+                    titleText = elem.getAttribute('aria-label') || '';
+                }
+                return titleText;
+            }
+
+            const elems = document.querySelectorAll('.datepicker_input');
+            for (const elem of elems) {
+                const datepicker = new Datepicker(elem, {
+                    'format': 'dd/mm/yyyy', // UK format
+                    title: getDatePickerTitle(elem)
+                });
+            }   
+// let jsonObj = {"person":"me","age":"30"};
+// console.log(Object.keys(jsonObj))
+
+
