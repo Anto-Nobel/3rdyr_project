830f74404abfbcdc43e004328711950437af729f
Your Name <anto1013.ei@rmkec.ac.in>
2022-01-07 15:30:32 +0530

    Co-authored-by: PraveenKanth1802 <PraveenKanth1802@users.noreply.github.com>

--- firebase_project/public/app.js
+++ firebase_project/public/app.js
@@ -1,50 +1,23 @@
-(function() {
-  const config = {
-  apiKey: "AIzaSyAcSi97LtykSiVO3VPBLmXA_hj5HXOsLLA",
-  authDomain: "espdemofirebase.firebaseapp.com",
-  databaseURL: "https://espdemofirebase-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "espdemofirebase",
-  storageBucket: "espdemofirebase.appspot.com",
-  messagingSenderId: "962438629865",
-  appId: "1:962438629865:web:03abe643e5f862d5476378"
+//(function() {
+  import  {initializeApp}  from 'firebase/app';
+  //import { getDatabase,ref } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js';
+
+  // TODO: Replace the following with your app's Firebase project configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAcSi97LtykSiVO3VPBLmXA_hj5HXOsLLA",
+    authDomain: "espdemofirebase.firebaseapp.com",
+    databaseURL: "https://espdemofirebase-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "espdemofirebase",
+    storageBucket: "espdemofirebase.appspot.com",
+    messagingSenderId: "962438629865",
+    appId: "1:962438629865:web:03abe643e5f862d5476378"
   };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
-  firebase.initializeApp(config);
-  //const bigTextEvaluationStudents = document.getElementById('temperatureTable');
-  const tempTable = document.getElementById('tempTable');
-  const rootRef = firebase.database().ref();
-  const tempRef=rootRef.child('test1').child('temperature');
-  const dateRef=tempRef.equalTo('02-01-2022').orderByKey();
+  const dB=firebase.database().ref();
 
-  let tempKey=[];
-  let tempVal=[]; 
-  dateRef.on('02-01-2022')
-  .then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      // key will be "ada" the first time and "alan" the second time
-      var tempKeyData = childSnapshot.key;
-      tempKey.push(tempKeyData);
-      // childData will be the actual contents of the child
-      var tempValData = childSnapshot.val();
-      tempVal.push(tempValData);
-  });
-});
-  //dbRef.on('value', snap => bigTextEvaluationStudents.innerText = snap.val());
-
-  var table = document.querySelector('#tab1 tbody');
-  //const dbEvaluationStudentsRef = firebase.database().ref().child('evaluationStudents');
-  dbEvaluationStudentsRef.on('value', snap => {
-    while(table.hasChildNodes()) {
-		    table.removeChild(table.firstChild);
-	  }
-
-    var students = snap.val();
-    for(var i in students) {
-      var row = table.insertRow(-1);
-      for(var j in students[i]) {
-				cell = row.insertCell(-1);
-				cell.innerHTML = students[i][j];
-			}
-		}
-  });
-}());
+  const rootRef=dB.ref();
+  console.log(rootRef);
+//}());
