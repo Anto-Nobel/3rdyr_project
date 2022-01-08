772a75848eeda578d83fe0dcd7b0b7d0f519b8a4
PraveenKanth1802 <92263475+PraveenKanth1802@users.noreply.github.com>
2022-01-08 18:06:15 +0530

    Final v0.0.0

---
+++ finalProject/public/index.js
@@ -0,0 +1,5 @@
+// let userDate = "2022-01-15";
+// let split = userDate.split("-");
+// let currentFormatUserDate = `${split[2]}-${split[1]}-${split[0]}`
+// console.log(split);
+// console.log(currentFormatUserDate);
