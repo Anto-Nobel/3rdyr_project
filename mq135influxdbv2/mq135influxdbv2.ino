25c2e691dcc8fba8321e0d15868f1352eab14c92
Anto-Nobel <nobel2002@gmail.com>
2022-04-23 11:59:21 +0530

    mq135 influxdb test v2

---
+++ mq135influxdbv2/mq135influxdbv2.ino
@@ -0,0 +1,183 @@
+#include <WiFiMulti.h>
+WiFiMulti wifiMulti;
+#define DEVICE "ESP32_1"
+#include <MQUnifiedsensor.h>
+
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
+#include <MQUnifiedsensor.h>
+
+#define WIFI_SSID "NaN"
+#define WIFI_PASSWORD "antonobel"
+
+//InfluxDB
+#define INFLUXDB_URL "https://europe-west1-1.gcp.cloud2.influxdata.com"
+#define INFLUXDB_TOKEN "KlmcztU1dEWZ04JzoiK3eg2eBtZxH-LbbYauBEH0UGpdmfjqxGvwIpaAIXv8EqTwIiXD-Hs0O3Konu3NjyrQfA=="
+#define INFLUXDB_ORG "nobel2002@gmail.com"
+#define INFLUXDB_BUCKET "nobel2002's Bucket"
+#define TZ_INFO "IST"
+
+
+//Definitions
+#define placa "ESP-32"
+#define Voltage_Resolution 3.3
+#define pin 4 //Analog input 0 of your arduino
+#define type "MQ-135" //MQ135
+#define ADC_Bit_Resolution 12 // For arduino UNO/MEGA/NANO
+#define RatioMQ135CleanAir 3.6//RS / R0 = 3.6 ppm  
+//#define calibration_button 13 //Pin to calibrate your sensor
+
+//Declare Sensor
+MQUnifiedsensor MQ135(placa, Voltage_Resolution, ADC_Bit_Resolution, pin, type);
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
+
+Point sensor1("pollutionReading");
+Point sensor2("pollutionReading");
+Point sensor3("pollutionReading");
+
+void influxInit(){
+    WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting to wifi");
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  sensor1.addTag("sensor", DEVICE);
+  sensor1.addTag("pollutant", "CO2");
+
+  sensor2.addTag("sensor", DEVICE);
+  sensor2.addTag("pollutant", "CO");
+
+  sensor3.addTag("sensor", DEVICE);
+  sensor3.addTag("pollutant", "NH4");
+
+  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+}
+
+void setup() {
+  //Init the serial port communication - to debug the library
+  Serial.begin(9600); //Init serial port
+
+  //Set math model to calculate the PPM concentration and the value of constants
+  MQ135.setRegressionMethod(1); //_PPM =  a*ratio^b
+  
+  /*****************************  MQ Init ********************************************/ 
+  //Remarks: Configure the pin of arduino as input.
+  /************************************************************************************/ 
+  MQ135.init(); 
+   
+    //If the RL value is different from 10K please assign your RL value with the following method:
+    MQ135.setRL(1);
+  
+  /*****************************  MQ CAlibration ********************************************/ 
+  // Explanation: 
+  // In this routine the sensor will measure the resistance of the sensor supposing before was pre-heated
+  // and now is on clean air (Calibration conditions), and it will setup R0 value.
+  // We recomend execute this routine only on setup or on the laboratory and save on the eeprom of your arduino
+  // This routine not need to execute to every restart, you can load your R0 if you know the value
+  // Acknowledgements: https://jayconsystems.com/blog/understanding-a-gas-sensor
+  Serial.print("Calibrating please wait.");
+  float calcR0 = 0;
+  for(int i = 1; i<=10; i ++)
+  {
+    MQ135.update(); // Update data, the arduino will be read the voltage on the analog pin
+    calcR0 += MQ135.calibrate(RatioMQ135CleanAir);
+    Serial.print(".");
+  }
+  MQ135.setR0(calcR0/10);
+  Serial.println("  done!.");
+  
+  if(isinf(calcR0)) {Serial.println("Warning: Conection issue founded, R0 is infite (Open circuit detected) please check your wiring and supply"); while(1);}
+  if(calcR0 == 0){Serial.println("Warning: Conection issue founded, R0 is zero (Analog pin with short circuit to ground) please check your wiring and supply"); while(1);}
+  /*****************************  MQ CAlibration ********************************************/ 
+  Serial.println("** Lectures from MQ-135 ****");
+  Serial.println("|    CO   |  Alcohol |   CO2  |  Tolueno  |  NH4  |  Acteona  |");  
+  influxInit();
+}
+
+void loop() {
+  MQ135.update(); // Update data, the arduino will be read the voltage on the analog pin
+
+  MQ135.setA(605.18); MQ135.setB(-3.937); // Configurate the ecuation values to get CO concentration
+  float CO = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  MQ135.setA(77.255); MQ135.setB(-3.18); // Configurate the ecuation values to get Alcohol concentration
+  float Alcohol = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  MQ135.setA(110.47); MQ135.setB(-2.862); // Configurate the ecuation values to get CO2 concentration
+  float CO2 = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  MQ135.setA(44.947); MQ135.setB(-3.445); // Configurate the ecuation values to get Tolueno concentration
+  float Tolueno = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  MQ135.setA(102.2 ); MQ135.setB(-2.473); // Configurate the ecuation values to get NH4 concentration
+  float NH4 = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  MQ135.setA(34.668); MQ135.setB(-3.369); // Configurate the ecuation values to get Acetona concentration
+  float Acetona = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+sensor1.clearFields();
+  sensor2.clearFields();
+  sensor3.clearFields();
+
+sensor1.addField("reading",CO2);
+  Serial.print("Writing: ");
+  Serial.println(sensor1.toLineProtocol());
+  if (!client.writePoint(sensor1)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  sensor2.addField("reading",CO);
+  Serial.print("Writing: ");
+  Serial.println(sensor2.toLineProtocol());
+  if (!client.writePoint(sensor2)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  sensor3.addField("reading",NH4);
+  Serial.print("Writing: ");
+  Serial.println(sensor3.toLineProtocol());
+  if (!client.writePoint(sensor3)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  Serial.println("Wait 1s");
+
+    
+  /*
+  Serial.print("|   "); Serial.print(CO); 
+  Serial.print("   |   "); Serial.print(Alcohol); 
+  Serial.print("   |   "); Serial.print(CO2+415.08); 
+  Serial.print("   |   "); Serial.print(Tolueno); 
+  Serial.print("   |   "); Serial.print(NH4); 
+  Serial.print("   |   "); Serial.print(Acetona);
+  Serial.println("   |"); 
+  */
+  /*
+    Exponential regression:
+  GAS      | a      | b
+  CO       | 605.18 | -3.937  
+  Alcohol  | 77.255 | -3.18 
+  CO2      | 110.47 | -2.862
+  Tolueno  | 44.947 | -3.445
+  NH4      | 102.2  | -2.473
+  Acetona  | 34.668 | -3.369
+  */
+
+  delay(500); //Sampling frequency
+}
