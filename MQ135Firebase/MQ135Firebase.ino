040acd37b3f243c3def15bddcf5982809c13770c
Your Name <anto1013.ei@rmkec.ac.in>
2022-04-26 19:49:56 +0530

    mq135 firebase

---
+++ MQ135Firebase/MQ135Firebase.ino
@@ -0,0 +1,222 @@
+#include <MQUnifiedsensor.h>
+#include <Arduino.h>
+#ifdef ESP32
+  #include <WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+
+#include <SPI.h> 
+#include <Wire.h> 
+#include <ArduinoJson.h>
+#include <DHT.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+#include "time.h"
+
+
+#define WIFI_SSID "Arunkumar M"
+#define WIFI_PASSWORD "arun_2002"
+
+#define API_KEY "AIzaSyD5YMCzj3aZIKbGxgBzSeei8kWK-ePU0wA"
+#define DATABASE_URL "https://test-d6170-default-rtdb.asia-southeast1.firebasedatabase.app/"  
+
+#define USER_EMAIL "dqwrqreho@gmail.com"
+#define USER_PASSWORD "ImITa@2021"
+
+#define placa "ESP-32"
+#define Voltage_Resolution 3.3
+#define pin 4 
+#define type "MQ-135"
+#define ADC_Bit_Resolution 12 
+#define RatioMQ135CleanAir 3.6
+
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+bool signupOK=false;
+String uid;
+//Declare Sensor
+MQUnifiedsensor MQ135(placa, Voltage_Resolution, ADC_Bit_Resolution, pin, type);
+unsigned long sendDataPrevMillis = 0;
+
+//NTP setup
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 19800;
+const int daylightOffset_sec = 3600;
+
+void initWiFi(){
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
+void setup() {
+  //Init the serial port communication - to debug the library
+  Serial.begin(9600); //Init serial port
+
+  //Set math model to calculate the PPM concentration and the value of constants
+  MQ135.setRegressionMethod(1); //_PPM =  a*ratio^b
+  
+  /*****************************  MQ Init ********************************************/ 
+  //Remarks: Configure the pin of arduino as input.
+  /************************************************************************************/ 
+  MQ135.init(); 
+   
+    //If the RL value is different from 10K please assign your RL value with the following method:
+    MQ135.setRL(1);
+  
+  /*****************************  MQ CAlibration ********************************************/ 
+  // Explanation: 
+  // In this routine the sensor will measure the resistance of the sensor supposing before was pre-heated
+  // and now is on clean air (Calibration conditions), and it will setup R0 value.
+  // We recomend execute this routine only on setup or on the laboratory and save on the eeprom of your arduino
+  // This routine not need to execute to every restart, you can load your R0 if you know the value
+  // Acknowledgements: https://jayconsystems.com/blog/understanding-a-gas-sensor
+  Serial.print("Calibrating please wait.");
+  float calcR0 = 0;
+  for(int i = 1; i<=10; i ++)
+  {
+    MQ135.update(); // Update data, the arduino will be read the voltage on the analog pin
+    calcR0 += MQ135.calibrate(RatioMQ135CleanAir);
+    Serial.print(".");
+  }
+  MQ135.setR0(calcR0/10);
+  Serial.println("  done!.");
+  
+  if(isinf(calcR0)) {Serial.println("Warning: Conection issue founded, R0 is infite (Open circuit detected) please check your wiring and supply"); while(1);}
+  if(calcR0 == 0){Serial.println("Warning: Conection issue founded, R0 is zero (Analog pin with short circuit to ground) please check your wiring and supply"); while(1);}
+  /*****************************  MQ CAlibration ********************************************/ 
+  Serial.println("** Lectures from MQ-135 ****");
+  Serial.println("|    CO   |  Alcohol |   CO2  |  Tolueno  |  NH4  |  Acteona  |");
+
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  
+  config.api_key = API_KEY;
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  Firebase.reconnectWiFi(true);
+  fbdo.setResponseSize(4096);
+
+  // Assign the callback function for the long running token generation task
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 5;
+
+  // Initialize the library with the Firebase authen and config
+  Firebase.begin(&config, &auth);
+
+  // Getting the user UID might take a few seconds
+  Serial.println("Getting User UID");
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  } 
+  signupOK = true;
+  // Print user UID
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.print(uid);
+}
+String convertToString(char* a, int size)
+{
+    int i;
+    String s = "";
+    for (i = 0; i < size; i++) {
+        s = s + a[i];
+    }
+    return s;
+}
+
+String tellTime()
+{
+  struct tm timeinfo;
+  getLocalTime(&timeinfo);
+  char a[22];
+  strftime(a,22, "%X", &timeinfo);
+  String t=convertToString(a,22);
+  return t;
+}
+
+String tellDate()
+{
+  struct tm timeinfo;
+  getLocalTime(&timeinfo);
+  char a[22];
+  strftime(a,22, "%d-%m-%Y", &timeinfo);
+  String t=convertToString(a,22);
+  return t;
+} 
+void loop() {
+  MQ135.update(); // Update data, the arduino will be read the voltage on the analog pin
+
+  MQ135.setA(605.18); MQ135.setB(-3.937); // Configurate the ecuation values to get CO concentration
+  float CO = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  MQ135.setA(77.255); MQ135.setB(-3.18); // Configurate the ecuation values to get Alcohol concentration
+  float Alcohol = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  MQ135.setA(110.47); MQ135.setB(-2.862); // Configurate the ecuation values to get CO2 concentration
+  float CO2 = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  MQ135.setA(44.947); MQ135.setB(-3.445); // Configurate the ecuation values to get Tolueno concentration
+  float Tolueno = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  MQ135.setA(102.2 ); MQ135.setB(-2.473); // Configurate the ecuation values to get NH4 concentration
+  float NH4 = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  MQ135.setA(34.668); MQ135.setB(-3.369); // Configurate the ecuation values to get Acetona concentration
+  float Acetona = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  Serial.print("|   "); Serial.print(CO); 
+  Serial.print("   |   "); Serial.print(Alcohol); 
+  Serial.print("   |   "); Serial.print(CO2+415.08); 
+  Serial.print("   |   "); Serial.print(Tolueno); 
+  Serial.print("   |   "); Serial.print(NH4); 
+  Serial.print("   |   "); Serial.print(Acetona);
+  Serial.println("   |"); 
+   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 1000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    String timeStamp=tellTime();
+    String currDate=tellDate();
+    FirebaseJson json1;
+    if(!(Firebase.RTDB.getJSON(&fbdo,"sensor_1/CO/"+currDate)))
+    {
+      json1.set(currDate+"/"+timeStamp,CO);
+      Serial.printf("Set json... %s\n", Firebase.RTDB.updateNode(&fbdo, F("/sensor_1/CO"), &json1) ? "ok" : fbdo.errorReason().c_str());
+    }
+    else
+    {
+      json1.add(timeStamp,CO);
+      Serial.printf("Update node... %s\n", Firebase.RTDB.updateNode(&fbdo, "sensor_1/CO/"+currDate, &json1) ? "ok" : fbdo.errorReason().c_str());
+    }
+
+    FirebaseJson json2;
+    if(!(Firebase.RTDB.getJSON(&fbdo,"sensor_1/CO2/"+currDate)))
+    {
+      json2.set(currDate+"/"+timeStamp,CO2);
+      Serial.printf("Set json... %s\n", Firebase.RTDB.updateNode(&fbdo, F("/sensor_1/CO2"), &json2) ? "ok" : fbdo.errorReason().c_str());
+    }
+    else
+    {
+      json2.add(timeStamp,CO2);
+      Serial.printf("Update node... %s\n", Firebase.RTDB.updateNode(&fbdo, "sensor_1/CO2/"+currDate, &json2) ? "ok" : fbdo.errorReason().c_str());
+    }
+   }
+
+
+  delay(500); //Sampling frequency
+}
